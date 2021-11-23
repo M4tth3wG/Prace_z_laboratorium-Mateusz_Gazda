116de12bd9c541086caef25d758e9b294028aa88
M4tth3wG <90974216+M4tth3wG@users.noreply.github.com>
2021-11-23 17:08:57 +0100

    Dodanie Lab1

---
+++ MateuszGazdaLab1/MateuszGazdaLab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MateuszGazdaLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadź tekst: ");
+            string tekst = Console.ReadLine();
+
+            Console.Write("Wprowadź pierwszą liczbę: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Wprowadź drugą liczbę: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Twój tekst to: " + tekst);
+            Console.WriteLine("Twoja pierwsza liczba to " + a);
+            Console.WriteLine("Twoja druga liczba to " + b);
+
+            Console.WriteLine($"{a} + {b} = {a+b}");
+            Console.WriteLine($"{a} - {b} = {a-b}");
+            Console.WriteLine($"{a} * {b} = {a*b}");
+            Console.WriteLine($"{a} / {b} = {a/b}");
+        }
+    }
+}
